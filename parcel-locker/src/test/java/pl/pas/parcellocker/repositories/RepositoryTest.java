980799d347a2a9759a2a2464d7a0bd658d36f5cb
WiktorPawlak <pawlak4112@wp.pl>
2022-10-19 23:16:51 +0200

    optimistic lock test

--- parcel-locker/src/test/java/pl/pas/parcellocker/repositories/RepositoryTest.java
+++ parcel-locker/src/test/java/pl/pas/parcellocker/repositories/RepositoryTest.java
@@ -1,17 +1,24 @@
 package pl.pas.parcellocker.repositories;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.NoResultException;
-import org.junit.jupiter.api.AfterEach;
+import jakarta.persistence.OptimisticLockException;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.RollbackException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.pas.parcellocker.config.TestsConfig;
 import pl.pas.parcellocker.model.Client;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RepositoryTest extends TestsConfig {
 
-    public Repository<Client> clientRepository = new Repository<>(Client.class);;
+    public Repository<Client> clientRepository = new Repository<>(Client.class);
+    ;
     public Client c;
     public Client c1;
 
@@ -38,7 +45,6 @@ class RepositoryTest extends TestsConfig {
     void findByConformance() {
         clientRepository.add(c);
         clientRepository.add(c1);
-        clientRepository.add(c);
         assertEquals(c1, clientRepository.findBy(client -> client.getLastName().equals("Kowalski")).get(0));
     }
 
@@ -46,8 +52,33 @@ class RepositoryTest extends TestsConfig {
     void findAllConformance() {
         clientRepository.add(c);
         clientRepository.add(c1);
-        clientRepository.add(c);
         assertTrue(2 <= clientRepository.findAll().size());
+    }
 
+    @Test
+    void triggersOptimisticLockWhenUnregisteringClientTwice() {
+        EntityManagerFactory emf1 = Persistence.createEntityManagerFactory("parcel-locker-unit");
+        EntityManager em1 = emf1.createEntityManager();
+
+        EntityManager em2 = emf1.createEntityManager();
+
+        clientRepository.add(c);
+
+        Client client1 = em1.find(Client.class, c.getId());
+        Client client2 = em2.find(Client.class, c.getId());
+
+        em1.getTransaction().begin();
+        client1.setArchive(true);
+        em1.getTransaction().commit();
+
+        RollbackException rollback = new RollbackException();
+        try {
+            em2.getTransaction().begin();
+            client2.setArchive(true);
+            em2.getTransaction().commit();
+        } catch (RollbackException e) {
+            rollback = e;
+        }
+        assertTrue(rollback.getCause() instanceof OptimisticLockException);
     }
 }
