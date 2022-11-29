16488eeab88114b1712a3827afe2616341fbbbbc
236665 Strzelecki Mateusz <236665@edu.p.lodz.pl>
2022-11-29 23:08:52 +0100

    Add properties file

--- parcel-locker/src/main/java/pl/pas/parcellocker/repositories/AbstractRedisRepository.java
+++ parcel-locker/src/main/java/pl/pas/parcellocker/repositories/AbstractRedisRepository.java
@@ -3,6 +3,7 @@ package pl.pas.parcellocker.repositories;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import pl.pas.parcellocker.model.MongoEntityModel;
+import pl.pas.parcellocker.utils.PropertiesLoader;
 import redis.clients.jedis.DefaultJedisClientConfig;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisClientConfig;
@@ -13,48 +14,50 @@ import java.util.UUID;
 
 public abstract class AbstractRedisRepository<T extends MongoEntityModel> implements AutoCloseable {
 
-    private static JedisPooled pool;
-    private Jsonb jsonb = JsonbBuilder.create();
-    private final Class<T> entityClass;
-    private String prefix;
+  private static JedisPooled pool;
+  private final Class<T> entityClass;
+  private final Jsonb jsonb = JsonbBuilder.create();
+  private final String prefix;
 
-    public void initDbConnection() {
-        JedisClientConfig clientConfig = DefaultJedisClientConfig.builder().build();
-        pool = new JedisPooled(new HostAndPort("localhost", 6379), clientConfig);
+  public AbstractRedisRepository(Class<T> entityClass) {
+    this.entityClass = entityClass;
+    this.prefix = entityClass.getSimpleName() + ":";
+
+    initDbConnection();
+  }
+
+  public void initDbConnection() {
+    JedisClientConfig clientConfig = DefaultJedisClientConfig.builder().build();
+    pool =
+        new JedisPooled(
+            new HostAndPort(
+                PropertiesLoader.getProperty("redis.host"),
+                Integer.parseInt(PropertiesLoader.getProperty("redis.port"))),
+            clientConfig);
+  }
+
+  public void add(T object) {
+    String json = jsonb.toJson(object);
+    pool.set(prefix + object.getId(), json);
+    pool.expire(prefix + object.getId(), 3600);
+  }
+
+  public T findById(UUID id) {
+    String json = pool.get(prefix + id);
+    return jsonb.fromJson(json, entityClass);
+  }
+
+  public Set<String> findAllKeys() {
+    return pool.keys(prefix + "*");
+  }
+
+  public void clear() {
+    Set<String> keys = pool.keys(prefix + "*");
+    for (String key : keys) {
+      pool.del(key);
     }
+  }
 
-    public AbstractRedisRepository(Class<T> entityClass) {
-        this.entityClass = entityClass;
-        this.prefix = entityClass.getSimpleName() + ":";
-
-        initDbConnection();
-    }
-
-    public void add(T object) {
-        String json = jsonb.toJson(object);
-        pool.set(prefix + object.getId(), json);
-        pool.expire(prefix + object.getId(), 3600);
-    }
-
-    public T findById(UUID id) {
-        String json = pool.get(prefix + id);
-        return  jsonb.fromJson(json, entityClass);
-    }
-
-    public Set<String> findAllKeys() {
-      return  pool.keys(prefix + "*");
-    }
-
-    public void clear() {
-        Set<String> keys = pool.keys(prefix + "*");
-        for (String key : keys) {
-            pool.del(key);
-        }
-    }
-
-    @Override
-    public void close() throws Exception {
-
-    }
-
+  @Override
+  public void close() throws Exception {}
 }
