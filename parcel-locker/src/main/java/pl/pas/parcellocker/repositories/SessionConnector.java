eae0c80a76d9bc78ad53778eed0d865364d00732
o-trela <boskichlopaczyna@gmail.com>
2022-12-28 17:35:09 +0100

    SessionConnector refactor

--- parcel-locker/src/main/java/pl/pas/parcellocker/repositories/SessionConnector.java
+++ parcel-locker/src/main/java/pl/pas/parcellocker/repositories/SessionConnector.java
@@ -2,11 +2,8 @@ package pl.pas.parcellocker.repositories;
 
 import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.datastax.oss.driver.api.core.CqlSession;
-import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 import com.datastax.oss.driver.api.core.type.DataTypes;
 import com.datastax.oss.driver.api.core.type.codec.ExtraTypeCodecs;
-import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
-import com.datastax.oss.driver.api.querybuilder.SchemaBuilder;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateKeyspace;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateMaterializedViewPrimaryKey;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateTable;
@@ -15,11 +12,13 @@ import pl.pas.parcellocker.model.DeliveryStatus;
 import java.net.InetSocketAddress;
 
 import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.createKeyspace;
+import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.createMaterializedView;
+import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.createTable;
 import static pl.pas.parcellocker.configuration.SchemaConst.PARCEL_LOCKER_NAMESPACE;
-
 
 public class SessionConnector implements AutoCloseable {
 
+    private final int REPLICATION_FACTOR = 2;
     public final CqlSession session;
 
     public SessionConnector() {
@@ -27,7 +26,6 @@ public class SessionConnector implements AutoCloseable {
     }
 
     private CqlSession initSession() {
-
         CqlSession session =
             CqlSession.builder()
                 .addContactPoint(new InetSocketAddress("127.22.0.2", 9042))
@@ -37,32 +35,51 @@ public class SessionConnector implements AutoCloseable {
                 .addTypeCodecs(ExtraTypeCodecs.enumNamesOf(DeliveryStatus.class))
                 .build();
 
-        CreateKeyspace keyspace =
-            createKeyspace(CqlIdentifier.fromCql(PARCEL_LOCKER_NAMESPACE))
-                .ifNotExists()
-                .withSimpleStrategy(2)
-                .withDurableWrites(true);
+        CreateKeyspace createKeyspace = prepareKeyspace(REPLICATION_FACTOR);
+        session.execute(createKeyspace.build());
 
-        SimpleStatement createKeyspace = keyspace.build();
-        session.execute(createKeyspace);
+        CreateTable createClientTable = prepareClientTable();
+        CreateTable createListTable = prepareListTable();
+        CreateTable createParcelTable = prepareParcelTable();
+        CreateTable createDeliveryByIdTable = prepareDeliveryByIdTable();
+        CreateMaterializedViewPrimaryKey createDeliveryByClientTable = prepareDeliveryByClientTable();
 
-        CreateTable clientTable = SchemaBuilder.createTable(PARCEL_LOCKER_NAMESPACE ,"clients")
+        session.execute(createClientTable.build());
+        session.execute(createListTable.build());
+        session.execute(createParcelTable.build());
+        session.execute(createDeliveryByIdTable.build());
+        session.execute(createDeliveryByClientTable.build());
+
+        return session;
+    }
+
+    private CreateKeyspace prepareKeyspace(int replicationFactor) {
+        return createKeyspace(CqlIdentifier.fromCql(PARCEL_LOCKER_NAMESPACE))
+            .ifNotExists()
+            .withSimpleStrategy(replicationFactor)
+            .withDurableWrites(true);
+    }
+
+    private CreateTable prepareClientTable() {
+        return createTable(PARCEL_LOCKER_NAMESPACE, "clients")
             .ifNotExists()
             .withPartitionKey("entity_id", DataTypes.UUID)
             .withColumn("first_name", DataTypes.TEXT)
             .withColumn("last_name", DataTypes.TEXT)
             .withColumn("tel_number", DataTypes.TEXT)
             .withClusteringColumn("active", DataTypes.BOOLEAN);
-        SimpleStatement createClientTable = clientTable.build();
+    }
 
-        CreateTable listTable = SchemaBuilder.createTable(PARCEL_LOCKER_NAMESPACE ,"list")
+    private CreateTable prepareListTable() {
+        return createTable(PARCEL_LOCKER_NAMESPACE, "list")
             .ifNotExists()
             .withPartitionKey("entity_id", DataTypes.UUID)
             .withColumn("base_price", DataTypes.DECIMAL)
             .withColumn("priority", DataTypes.BOOLEAN);
-        SimpleStatement createListTable = listTable.build();
+    }
 
-        CreateTable parcelTable = SchemaBuilder.createTable(PARCEL_LOCKER_NAMESPACE ,"parcel")
+    private CreateTable prepareParcelTable() {
+        return createTable(PARCEL_LOCKER_NAMESPACE, "parcel")
             .ifNotExists()
             .withPartitionKey("entity_id", DataTypes.UUID)
             .withColumn("base_price", DataTypes.DECIMAL)
@@ -71,9 +88,10 @@ public class SessionConnector implements AutoCloseable {
             .withColumn("height", DataTypes.DOUBLE)
             .withColumn("weight", DataTypes.DOUBLE)
             .withColumn("fragile", DataTypes.BOOLEAN);
-        SimpleStatement createParcelTable = parcelTable.build();
+    }
 
-        CreateTable deliveryByIdTable = SchemaBuilder.createTable(PARCEL_LOCKER_NAMESPACE ,"delivery_by_id")
+    private CreateTable prepareDeliveryByIdTable() {
+        return createTable(PARCEL_LOCKER_NAMESPACE, "delivery_by_id")
             .ifNotExists()
             .withPartitionKey("entity_id", DataTypes.UUID)
             .withColumn("shipper_id", DataTypes.UUID)
@@ -82,9 +100,10 @@ public class SessionConnector implements AutoCloseable {
             .withColumn("package_id", DataTypes.UUID)
             .withColumn("locker_id", DataTypes.UUID)
             .withColumn("archived", DataTypes.BOOLEAN);
-        SimpleStatement createDeliveryByIdTable = deliveryByIdTable.build();
+    }
 
-        CreateMaterializedViewPrimaryKey deliveryByClientTable = SchemaBuilder.createMaterializedView(PARCEL_LOCKER_NAMESPACE ,"delivery_by_client")
+    private CreateMaterializedViewPrimaryKey prepareDeliveryByClientTable() {
+        return createMaterializedView(PARCEL_LOCKER_NAMESPACE, "delivery_by_client")
             .ifNotExists()
             .asSelectFrom(PARCEL_LOCKER_NAMESPACE, "delivery_by_id")
             .columns("entity_id", "receiver_id", "archived")
@@ -96,19 +115,10 @@ public class SessionConnector implements AutoCloseable {
             .isNotNull()
             .withPartitionKey("receiver_id")
             .withClusteringColumn("entity_id");
-        SimpleStatement createDeliveryByClientTable = deliveryByClientTable.build();
-
-        session.execute(createClientTable);
-        session.execute(createListTable);
-        session.execute(createParcelTable);
-        session.execute(createDeliveryByIdTable);
-        session.execute(createDeliveryByClientTable);
-
-        return session;
     }
 
     @Override
-    public void close() throws Exception {
+    public void close() {
         session.close();
     }
 }
