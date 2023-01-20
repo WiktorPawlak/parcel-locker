9df1338fb8de406dd069d4df1107b354be4cb3f6
WiktorPawlak <pawlak4112@wp.pl>
2023-01-20 01:02:00 +0100

    extract classess to modules

--- parcel-locker/src/main/java/pl/pas/parcellocker/exceptions/DeliveryManagerException.java
+++
@@ -1,7 +0,0 @@
-package pl.pas.parcellocker.exceptions;
-
-public class DeliveryManagerException extends RuntimeException {
-    public DeliveryManagerException(String message) {
-        super(message);
-    }
-}
