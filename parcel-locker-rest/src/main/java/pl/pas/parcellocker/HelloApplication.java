52cb24935dc2a1971fad3f1ecc70707262d8bb4a
WiktorPawlak <pawlak4112@wp.pl>
2023-01-21 15:48:08 +0100

    init users&locker

--- parcel-locker-rest/src/main/java/pl/pas/parcellocker/HelloApplication.java
+++ parcel-locker-rest/src/main/java/pl/pas/parcellocker/HelloApplication.java
@@ -1,9 +1,17 @@
 package pl.pas.parcellocker;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
+import pl.pas.parcellocker.model.locker.Locker;
+import pl.pas.parcellocker.model.locker.LockerRepository;
+import pl.pas.parcellocker.model.user.Administrator;
+import pl.pas.parcellocker.model.user.Client;
+import pl.pas.parcellocker.model.user.Moderator;
+import pl.pas.parcellocker.model.user.User;
 import pl.pas.parcellocker.model.user.UserRepository;
+
 
 @ApplicationPath("/api")
 public class HelloApplication extends Application {
@@ -11,9 +19,26 @@ public class HelloApplication extends Application {
     @Inject
     private UserRepository userRepository;
 
-//    @PostConstruct
-//    void init() {
-//        User admin = new Administrator("admin", "admin", "admin");
-//        userRepository.add(admin);
-//    }
+    @Inject
+    private LockerRepository lockerRepository;
+
+    @PostConstruct
+    void init() {
+        if (!userRepository.findByTelNumber("666666666").isPresent()) {
+            User admin = new Administrator("admin", "admin", "666666666");
+            User moderator = new Moderator("moderator", "moderator", "546777890");
+            User client1 = new Client("Johnzon", "Jackson", "420213769");
+            User client2 = new Client("Yasson", "Jackson", "123456789");
+            User client3 = new Client("Geronimo", "Jackson", "234345456");
+            userRepository.add(admin);
+            userRepository.add(moderator);
+            userRepository.add(client1);
+            userRepository.add(client2);
+            userRepository.add(client3);
+        }
+
+        if (!lockerRepository.findByIdentityNumber("LODZ_01").isPresent()) {
+            lockerRepository.add(new Locker("LODZ_01", "XD", 10));
+        }
+    }
 }
