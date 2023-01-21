98659d8e5d21e24b0a678db68601d51351873b1e
o-trela <boskichlopaczyna@gmail.com>
2023-01-22 00:01:57 +0100

    co do kur

---
+++ parcel-locker-rest/src/test/java/pl/pas/parcellocker/repositories/hibernate/DeliveryRepositoryHibernateTest.java
@@ -0,0 +1,83 @@
+package pl.pas.parcellocker.repositories.hibernate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import jakarta.persistence.NoResultException;
+import pl.pas.parcellocker.config.TestsConfig;
+import pl.pas.parcellocker.model.delivery.Delivery;
+import pl.pas.parcellocker.model.delivery.DeliveryStatus;
+import pl.pas.parcellocker.model.locker.Locker;
+import pl.pas.parcellocker.model.user.Client;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class DeliveryRepositoryHibernateTest extends TestsConfig {
+
+    private Client c1;
+    private Client c2;
+    private Locker l1;
+
+    @BeforeAll
+    void setup() {
+        l1 = new Locker("LDZ01", "Gawronska 12, Lodz 12-123", 10);
+        c1 = new Client("Maciej", "Nowak", "12345", "P@ssw0rd");
+        c2 = new Client("Maciej", "Kowal", "123456", "P@ssw0rd");
+        clientRepository.add(c1);
+        clientRepository.add(c2);
+        lockerRepository.add(l1);
+    }
+
+    @Test
+    void Should_CreateDelivery() {
+        Delivery delivery = new Delivery(BigDecimal.TEN, true, c1, c2, l1);
+        deliveryRepository.add(delivery);
+        delivery = deliveryRepository.get(delivery.getId());
+        assertEquals(deliveryRepository.get(delivery.getId()), delivery);
+    }
+
+    @Test
+    void Should_UpdateDelivery() {
+        Delivery delivery = new Delivery(BigDecimal.TEN, true, c1, c2, l1);
+        deliveryRepository.add(delivery);
+        assertEquals(deliveryRepository.get(delivery.getId()).getStatus(), DeliveryStatus.READY_TO_SHIP);
+        delivery.setStatus(DeliveryStatus.READY_TO_PICKUP);
+        deliveryRepository.update(delivery);
+        assertEquals(deliveryRepository.get(delivery.getId()).getStatus(), DeliveryStatus.READY_TO_PICKUP);
+    }
+
+    @Test
+    void Should_DeleteDelivery() {
+        Delivery delivery = new Delivery(BigDecimal.TEN, true, c1, c2, l1);
+        deliveryRepository.add(delivery);
+        assertEquals(deliveryRepository.get(delivery.getId()), delivery);
+        deliveryRepository.remove(delivery);
+        assertThrows(NoResultException.class, () -> deliveryRepository.get(delivery.getId()));
+    }
+
+    @Test
+    void Should_ArchiveDelivery() {
+        Delivery delivery = new Delivery(BigDecimal.TEN, true, c1, c2, l1);
+        deliveryRepository.add(delivery);
+        assertFalse(deliveryRepository.get(delivery.getId()).isArchived());
+        deliveryRepository.archive(delivery.getId());
+        assertTrue(deliveryRepository.get(delivery.getId()).isArchived());
+    }
+
+    @Test
+    void Should_ReturnAllDeliveries() {
+        Delivery delivery = new Delivery(BigDecimal.TEN, true, c1, c2, l1);
+        Delivery delivery1 = new Delivery(BigDecimal.ONE, false, c2, c1, l1);
+        deliveryRepository.add(delivery);
+        deliveryRepository.add(delivery1);
+        assertTrue(deliveryRepository.findAll().contains(delivery));
+        assertTrue(deliveryRepository.findAll().contains(delivery1));
+    }
+}
---
+++ parcel-locker-rest/src/test/java/pl/pas/parcellocker/repositories/hibernate/RepositoryTest.java
@@ -0,0 +1,96 @@
+package pl.pas.parcellocker.repositories.hibernate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.OptimisticLockException;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.RollbackException;
+import pl.pas.parcellocker.config.TestsConfig;
+import pl.pas.parcellocker.model.user.Client;
+import pl.pas.parcellocker.model.user.User;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class RepositoryTest extends TestsConfig {
+
+    private final HibernateRepository<User> clientRepository = new HibernateRepository<>(User.class);
+    private User c1;
+    private User c2;
+
+    @BeforeEach
+    void setup() {
+        c1 = new Client("Tadeusz", "Kaczmarski", "606123654", "P@ssw0rd");
+        c2 = new Client("Pawel", "Tubiel", "606444654", "P@ssw0rd");
+    }
+
+    @AfterEach
+    void finisher() {
+        clientRepository.findAll().forEach(clientRepository::remove);
+    }
+
+    @Test
+    void Should_AddAndGetClient() {
+        clientRepository.add(c1);
+        assertEquals(c1, clientRepository.get(c1.getId()));
+    }
+
+    @Test
+    void Should_RemoveClient() {
+        clientRepository.add(c1);
+        clientRepository.remove(c1);
+        assertThrows(NoResultException.class, () -> clientRepository.get(c1.getId()));
+    }
+
+    @Test
+    void Should_FindClient_WhenPredicateGivenToFindByMethod() {
+        clientRepository.add(c1);
+        clientRepository.add(c2);
+        assertEquals(c2, clientRepository.findBy(client -> client.getLastName().equals("Tubiel")).get(0));
+    }
+
+    @Test
+    void Should_ReturnAllClients() {
+        clientRepository.add(c1);
+        clientRepository.add(c2);
+        assertTrue(2 <= clientRepository.findAll().size());
+        assertTrue(clientRepository.findAll().containsAll(List.of(c1, c2)));
+    }
+
+    @Test
+    void Should_TriggerOptimisticLock_WhenUnregisteringClientTwice() {
+        EntityManagerFactory emf1 = Persistence.createEntityManagerFactory("parcel-locker-unit");
+        EntityManager em1 = emf1.createEntityManager();
+
+        EntityManager em2 = emf1.createEntityManager();
+
+        clientRepository.add(c1);
+
+        User user1 = em1.find(User.class, c1.getId());
+        User user2 = em2.find(User.class, c1.getId());
+
+        em1.getTransaction().begin();
+        user1.setActive(false);
+        em1.getTransaction().commit();
+
+        RollbackException rollback = new RollbackException();
+        try {
+            em2.getTransaction().begin();
+            user2.setActive(false);
+            em2.getTransaction().commit();
+        } catch (RollbackException e) {
+            rollback = e;
+        }
+        assertTrue(rollback.getCause() instanceof OptimisticLockException);
+    }
+}
